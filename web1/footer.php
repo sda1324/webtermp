ea3745ef17eff554698297602b4899d09b50fb10
dnwls <hjkl00114@gmail.com>
2018-06-06 15:22:34 +0900

    init

---
+++ web1/footer.php
@@ -0,0 +1,6 @@
+            <ul>
+                <li> <a href="index.html">Base</a> </li>
+                <li> <a href="inline.html">Inline CSS</a> </li>
+                <li> <a href="embedded.html">Embedded CSS</a> </li>
+                <li> <a href="external.html">EXternal CSS</a> </li>
+            </ul>
